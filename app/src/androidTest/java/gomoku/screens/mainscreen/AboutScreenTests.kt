3b29b8722d00f917c646e792c1405e22b2d19757
Tiago <a48666@alunos.isel.pt>
2023-11-06 21:54:56 +0000

    Added the Tests , and in the code added testTag to modifier please Do Not Remove them its important to the tests

--- app/src/androidTest/java/gomoku/screens/mainscreen/AboutScreenTests.kt
+++ app/src/androidTest/java/gomoku/screens/mainscreen/AboutScreenTests.kt
@@ -1,7 +1,12 @@
 package gomoku.screens.mainscreen
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.annotation.ExperimentalTestApi
+import gomoku.about.domain.About
+import gomoku.about.ui.AboutScreen
+import gomoku.ui.components.test
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,14 +19,39 @@ class AboutScreenTests {
     @Test
     fun click_on_game_information_expands_the_Card() {
         // Arrange
-        //composeTestRule.setContent {
-          //  AboutScreen(
-            //    onBurgerMenuClick = { /*TODO*/ },
-             //   sections = About.sections
-           // )
-       // }
+        composeTestRule.setContent {
+            AboutScreen(
+                onBurgerMenuClick = { /*TODO*/ },
+                sections = About.sections
+            )
+        }
         // Act
-        //composeTestRule.onNodeWithTag("Game Information").performClick()
-        // Assert
+        composeTestRule.onNodeWithTag("Game Information$test").performClick()
+    }
+
+    @Test
+    fun click_on_feedback_and_support_expands_the_Card() {
+        // Arrange
+        composeTestRule.setContent {
+            AboutScreen(
+                onBurgerMenuClick = { /*TODO*/ },
+                sections = About.sections
+            )
+        }
+        // Act
+        composeTestRule.onNodeWithTag("Feedback and Support$test").performClick()
+    }
+
+    @Test
+fun click_on_about_the_developer_expands_the_Card() {
+        // Arrange
+        composeTestRule.setContent {
+            AboutScreen(
+                onBurgerMenuClick = { /*TODO*/ },
+                sections = About.sections
+            )
+        }
+        // Act
+        composeTestRule.onNodeWithTag("Authors$test").performClick()
     }
 }
---
+++ app/src/androidTest/java/gomoku/screens/mainscreen/LeaderBoardScreenTests.kt
@@ -0,0 +1,11 @@
+package gomoku.screens.mainscreen
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class LeaderBoardScreenTests {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    //TODO(not done because i need to talk with teacher first to know how to do this test)
+}
